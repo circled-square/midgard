e9ea94bc6dcf213f4958de3b216c72cd8b0eb8c9
circled-square <circled-square@github.com>
2023-12-06 16:57:06 +0100

    elevation generation with multi octave perlin noise

--- src/world_generator.rs
+++ src/world_generator.rs
@@ -1,9 +1,10 @@
+mod multi_octave_noise;
+
 use bevy::{prelude::*, window::WindowResolution};
 use bevy_pixels::prelude::*;
+use noise::*;
+use multi_octave_noise::Multi;
 use robotics_lib::world::{worldgenerator::Generator, tile::Tile, tile::{TileType, Content}, environmental_conditions::{EnvironmentalConditions, WeatherType}};
-#[allow(unused_imports)]
-use noise::{Abs, Add, Billow, Blend, Constant, Curve, Multiply, Negate, NoiseFn, Perlin, ScalePoint, Seedable, Turbulence};
-use noise::Clamp;
 
 pub struct WorldGenerator {
     seed: u32,
@@ -14,7 +15,6 @@ pub struct WorldGenerator {
 struct WorldMatrixResource {
     matrix: Vec<Vec<Tile>>
 }
-
 impl WorldGenerator {
     pub fn new(seed: u32, world_size: usize) -> Self {
         Self { seed, world_size }
@@ -29,64 +29,46 @@ impl WorldGenerator {
     }
 
     fn generate_altitude(&self) -> Vec<Vec<f64>> {
-        let tamed_billow = |seed|
-            Negate::new(
-                Add::new(
-                    Multiply::new(
-                        ScalePoint::new(
-                            Billow::<Perlin>::new(seed)
-                        ).set_scale(1.0/50.0),
-                        Constant::new(2.2 / 3.64)
-                    ),
-                    Constant::new(0.2)
-                )
-            );
+        let noise_function = Multi::new(Perlin::new(self.seed), 7, 1.0/90.0);
 
-        let noise_function = tamed_billow(self.seed);
+        let noise_function = Multiply::new(Constant::new(1.5), noise_function);
 
-        let noise_function =
-            Add::new(
-                noise_function,
-                Multiply::new(
-                    ScalePoint::new(
-                        Perlin::new(self.seed+10)
-                    ).set_scale(1.0/100.0),
-                    Constant::new(0.5)
-                )
-            );
-        let noise_function = Clamp::new(noise_function).set_bounds(-1.0, 1.0);
+        // clamp values in [-1,1] range
+        // let noise_function = Clamp::new(noise_function).set_bounds(-1.0, 1.0);
+
         let mut elevation_map = vec![vec![0.0; self.world_size]; self.world_size];
 
+        let mut min = 10000.;
+        let mut max = -10000.;
         for x in 0..self.world_size {
             for y in 0..self.world_size {
                 let noise = noise_function.get([x as f64, y as f64]);
+                if noise > max { max = noise; }
+                if noise < min { min = noise; }
                 elevation_map[x][y] = noise;
             }
         }
+        println!("altitude range: = [{min}, {max}]");
 
         return elevation_map;
     }
 
     fn generate_biomes(&self, elevation_map: Vec<Vec<f64>>) -> Vec<Vec<Tile>> {
         let deep_water_tile = Tile { tile_type: TileType::DeepWater, content: Content::None, elevation: 0};
-        let shallow_water_tile = Tile { tile_type: TileType::ShallowWater, content: Content::None, elevation: 0};
-        let grass_tile = Tile { tile_type: TileType::Grass, content: Content::None, elevation: 0};
-        let hill_tile = Tile { tile_type: TileType::Hill, content: Content::None, elevation: 0};
-        let mountain_tile = Tile { tile_type: TileType::Mountain, content: Content::None, elevation: 0};
-        let snow_tile = Tile { tile_type: TileType::Snow, content: Content::None, elevation: 0};
-
-        let mut world = vec![vec![deep_water_tile.clone(); self.world_size]; self.world_size];
+        let mut world = vec![vec![deep_water_tile; self.world_size]; self.world_size];
 
         for x in 0..self.world_size {
             for y in 0..self.world_size {
-                world[x][y] = match elevation_map[x][y] {
-                    h if h < -0.65 => deep_water_tile.clone(),
-                    h if h < -0.55 => shallow_water_tile.clone(),
-                    h if h <  0.10 => grass_tile.clone(),
-                    h if h <  0.65 => hill_tile.clone(),
-                    h if h <  0.90 => mountain_tile.clone(),
-                    _ => snow_tile.clone(),
+                let tile_type = match elevation_map[x][y] {
+                    h if h < -0.65 => TileType::DeepWater,
+                    h if h < -0.40 => TileType::ShallowWater,
+                    h if h < -0.30 => TileType::Sand,
+                    h if h <  0.35 => TileType::Grass,
+                    h if h <  0.55 => TileType::Hill,
+                    h if h <  0.85 => TileType::Mountain,
+                    _ => TileType::Snow,
                 };
+                world[x][y] = Tile { tile_type, content: Content::None, elevation: 0 };
             }
         }
 
@@ -105,6 +87,7 @@ impl WorldGenerator {
             TileType::Hill => [0, 127, 0, 255],
             TileType::Mountain => [153, 102, 51, 255],
             TileType::Snow => [255, 255, 255, 255],
+            TileType::Sand => [192, 192, 0, 255],
             _ => [0, 0, 0, 255]
         }
     }
