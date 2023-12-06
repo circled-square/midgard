56ab210de5f8736a88f3884b79be4c59c373b7d0
Kalsifer <kalsifer742@gmail.com>
2023-12-06 23:41:38 +0100

    Randomized seed and added package for random object distribution

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use robotics_lib::world::worldgenerator::Generator;
 use world_generator::WorldGenerator;
 
 fn main() {
-    let mut world_generator = WorldGenerator::new(1, 100);
+    let mut world_generator = WorldGenerator::new(rand::random::<u32>(), 100);
     let (world, (_spawn_x, _spawn_y), _weather, _max_score) = world_generator.gen();
     WorldGenerator::visualize(world, 800);
 }
